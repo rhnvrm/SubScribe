f5701f8cec159480f780b73cdccf871318e17fe7
Rohan Verma <rohanverma2004@gmail.com>
2015-11-15 19:19:32 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,27 +4,26 @@ import pysrt
 def mid(x,y):
 	return (x+y)/2
 
-def save_new_frame(ms, text):
-	#ms = int(raw_input("What is the millisecond of the value you want to save?"))
-	#ms = 5000
-	videofile = 'test.mp4'
+def save_new_frame(file, ms, text):
 
 	videocapture = cv2.VideoCapture(videofile)
 	videocapture.set(cv2.cv.CV_CAP_PROP_POS_MSEC,ms)
 	success, image = videocapture.read()
 	
+	height, width = image.shape[:2]
+
 	if success:
 		font = cv2.FONT_HERSHEY_SIMPLEX
-		cv2.putText(image,'Hello World!',(10,500), font, 1,(255,255,255),2)
-		cv2.imwrite('frame%d.jpg' % (ms/1000), image)
+		cv2.putText(image,text,(10,height-10), font, 0.4,(255,255,255),1)
+		cv2.imwrite('frame%03d.jpg' % (ms/1000), image)
 		#cv2.imshow('frame%d % (ms)', image)
 	    #cv2.waitKey()	
 
-subs = pysrt.open('test.srt')
+if __name__ == "__main__":
 
-for i in xrange(0, len(subs)):
-	ms = mid(subs[i].start.seconds*1000,subs[i].end.seconds*1000)
-	save_new_frame(ms, "hello world")
+	subs = pysrt.open('comedian/test.srt')
+	videofile = 'comedian/test.mp4'
 
-
-
+	for i in xrange(0, len(subs)):
+		ms = mid(subs[i].start.seconds*1000,subs[i].end.seconds*1000)
+		save_new_frame(videofile, ms, str(subs[i].text))
